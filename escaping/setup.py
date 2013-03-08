5fa7c8261cc541381a94d0717bc1de58d80265cf
Gregorio Martin Romero <gmr@tid.es>
2013-03-08 10:44:59 +0100

    fallaban los setup.py con mis nuevos modulos xq no los encontraba. + el path __utils

--- escaping/setup.py
+++ escaping/setup.py
@@ -2,4 +2,5 @@ from distutils.core import setup
 import py2exe
 import sys
 sys.path.append("C:\Program Files (x86)\\Calibre2\\Microsoft.VC90.CRT")
+sys.path.append("../__utils")
 setup(console=['escaping.py'])
